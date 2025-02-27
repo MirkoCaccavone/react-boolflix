dd22e376c75bde1ee32ca4305b1f4bcfde78c88d
Mirko <mirkocaccavone03@gmail.com>
2025-02-27 16:54:12 +0100

    [feat] Add flag display functionality and new default image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,26 @@
 import { useContext } from "react";
 import GlobalContext from "../context/GlobalContext";
+import Flag from "react-world-flags";
 
 export default function Main() {
 
     // Accediamo ai film dal contesto
     const { movies } = useContext(GlobalContext);
+
+
+    // Funzione per determinare la bandiera dalla lingua
+    const getFlag = (language) => {
+        const flags = {
+            "en": "GB",
+            "it": "IT",
+            "es": "ES",
+
+        };
+
+        // Se il codice della lingua esiste, restituisce la bandiera corrispondente
+        return flags[language]
+    };
+
     return (
         <main>
             <h2>Risultati della ricerca</h2>
@@ -14,7 +30,16 @@ export default function Main() {
                         <li key={movie.id}>
                             <h3>{movie.title}</h3>
                             <p><strong>Titolo Originale:</strong> {movie.original_title}</p>
-                            <p><strong>Lingua:</strong> {movie.original_language}</p>
+                            <p><strong>Lingua:</strong>
+                                {getFlag(movie.original_language) ? (
+                                    <Flag
+                                        code={getFlag(movie.original_language)}
+                                        style={{ width: "30px", height: "20px" }} // Puoi personalizzare le dimensioni
+                                    />
+                                ) : (
+                                    <img src="./../public/bandiera_pirata.avif" alt="Flag not found" style={{ width: "30px", height: "20px" }} />
+                                )}
+                            </p>
                             <p><strong>Voto:</strong> {movie.vote_average}</p>
                         </li>
                     ))}
