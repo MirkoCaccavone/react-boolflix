4d153ee774b157f4ba806d2c99ce2649d13d7845
Mirko <mirkocaccavone03@gmail.com>
2025-02-27 19:52:20 +0100

    [feat] Add FontAwesome icons and star rating functionality

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,11 @@ import { useContext } from "react";
 import GlobalContext from "../context/GlobalContext";
 import Flag from "react-world-flags";
 
+// Importa il componente FontAwesomeIcon da FontAwesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons"; // Icona stella piena
+
+
 export default function Main() {
 
     // Accediamo ai film dal contesto
@@ -16,12 +21,28 @@ export default function Main() {
             "es": "ES",
 
         };
-
-
-
         // Se il codice della lingua esiste, restituisce la bandiera corrispondente
         return flags[language]
     };
+
+    // Funzione per generare le stelle in base al voto
+    const generateStars = (voteAverage) => {
+        const fullStars = Math.ceil(voteAverage / 2); // Arrotondiamo il voto diviso per 2
+        let stars = [];
+
+        // Genera le stelle
+        for (let i = 0; i < 5; i++) {
+            stars.push(
+                <FontAwesomeIcon
+                    key={i}
+                    icon={faStar} // Stella piena
+                    style={{ color: i < fullStars ? "#ffff80" : "#c0c0c0" }}
+                />
+            )
+        }
+        return stars;
+    };
+
 
     return (
         <main>
@@ -44,6 +65,7 @@ export default function Main() {
                                 )}
                             </p>
                             <p><strong>Voto:</strong> {movie.vote_average}</p>
+                            <div>{generateStars(movie.vote_average)}</div>
                             <p><strong>Tipo:</strong> {movie.type === "movie" ? "Film" : "Serie TV"}</p>
                         </li>
                     ))}
