f679148b996f5cbd7699cf1bebdea60ea137469d
Mirko <mirkocaccavone03@gmail.com>
2025-02-28 18:16:41 +0100

    [feat] Add MoviePage and SerieTvPage components

---
+++ src/pages/MoviePage.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import MovieComponents from "../components/MovieComponents";
+
+export default function MoviePage() {
+    return (
+
+        <div>
+            <Header />
+            <MovieComponents />
+
+        </div>
+
+    )
+}
---
+++ src/pages/SerieTvPage.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import SerieTvComponents from "../components/SerieTvComponents";
+
+export default function SerieTvPage() {
+    return (
+
+        <div>
+            <Header />
+            <SerieTvComponents />
+
+        </div>
+
+    )
+}
