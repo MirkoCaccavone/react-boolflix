c455258952a403e06a558a11eaedfcf49b46f1d0
Mirko <mirkocaccavone03@gmail.com>
2025-03-01 17:57:02 +0100

    [fix] Update index.html title, enhance MovieForm component with improved comments, change style in css

--- src/components/MovieForm.jsx
+++ src/components/MovieForm.jsx
@@ -1,24 +1,25 @@
 // Hook di React per gestire lo stato del componente
 import { useState, useContext } from "react";
-import GlobalContext from "./../context/GlobalContext"; // Importiamo il contesto
+
+// Importiamo il contesto globale
+import GlobalContext from "./../context/GlobalContext";
 
 // Riceviamo setMovies come prop
 const MovieForm = () => {
 
-    // Accediamo alla funzione searchMovies
+    // Accediamo alla funzione searchMoviesAndTV dal contesto globale
     const { searchMoviesAndTV } = useContext(GlobalContext);
 
-    // state per la gestione delle informazioni raccolte dai campi del form
+    // Stato per la gestione delle informazioni raccolte dai campi del form
     const [formData, setFormData] = useState({ title: "" });
 
-
-    // funzione di gestione delle info dei campi
+    // Funzione di gestione delle info dei campi
     function handleFormData(e) {
-
+        // Aggiorna lo stato con i dati del form
         setFormData({ ...formData, [e.target.name]: e.target.value });
     }
 
-    // funzione di gestione dell'invio dell'intero form per cercare il film
+    // Funzione di gestione dell'invio dell'intero form per cercare il film
     function handleSubmit(e) {
         e.preventDefault();
         // Eseguiamo la ricerca dei film e serie tv
@@ -30,7 +31,7 @@ const MovieForm = () => {
 
     return (
         <>
-
+            {/* Form per la ricerca dei film e delle serie TV */}
             <form id="formMovie" action="#" onSubmit={handleSubmit}>
                 <input
                     type="text"
